150941796c5fdcb0ad87b47cd76523effbaf8ede
MAHI1119 <mahijanu1119@gmail.com>
2021-08-04 17:33:25 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,163 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VueScrollFixedNavbar>
+      <header class="sticky-top">
+        <img
+          src="./assets/logo3.jpeg"
+          class="logo"
+          width="55px"
+          height="55px"
+        />
+        <h1>Musically</h1>
+        <span class="log">Login</span>
+      </header>
+    </VueScrollFixedNavbar>
+    <content>
+      <section class="player">
+        <center>
+          <img
+            src="https://images.unsplash.com/photo-1507676184212-d03ab07a01bf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNpbmdlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=300&q=200"
+            width="300px"
+            height="350px"
+            class="image"
+          />
+        </center>
+        <h2 class="title">
+          {{ current.movie }} - <span>{{ current.Song }}</span>
+        </h2>
+        <div class="controls">
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
+        </div>
+      </section>
+
+      <container class="playlist">
+        <h3>
+          The Playlist
+        </h3>
+
+        <span
+          v-for="song in songs"
+          :key="song.src"
+          @click="play(song)"
+          :class="song.src == current.src ? 'song playing' : 'song'"
+        >
+          {{ song.movie }} - {{ song.Song }}
+          <img
+            src="./assets/start2.png"
+            width="30px"
+            height="30px"
+            class="start"
+          />
+        </span>
+      </container>
+    </content>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+      songs: [
+        {
+          movie: "Teddy",
+          Song: "iniya thanimayey",
+
+          src: require("./assets/teddy.mp3"),
+        },
+        {
+          movie: "Teddy",
+          Song: "Marandaaye",
+
+          src: require("./assets/marandaayey.mp3"),
+        },
+        {
+          movie: "Master",
+          Song: " Kutty Story",
+
+          src: require("./assets/kutty story.mp3"),
+        },
+        {
+          movie: "Karnan",
+          Song: " Kandaa Vara",
+
+          src: require("./assets/Kandaa Vara.mp3"),
+        },
+        {
+          movie: "Album",
+          Song: "Shap of you ",
+
+          src: require("./assets/SHAPE OF YOU.mp3"),
+        },
+        {
+          movie: "Premam",
+          Song: "Malare ninne",
+
+          src: require("./assets/Malare-Ninne.mp3"),
+        },
+        {
+          movie: "Sultan",
+          Song: "Yaaraiyum Ivlo",
+
+          src: require("./assets/Yaaraiyum Ivlo Azhaga.mp3"),
+        },
+      ],
+      player: new Audio(),
+    };
+  },
+  methods: {
+    play(song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.player.addEventListener(
+        "ended",
+        function() {
+          this.index++;
+          if (this.index > this.songs.length - 1) {
+            this.index = 0;
+          }
+          this.current = this.songs[this.index];
+          this.play(this.current);
+        }.bind(this)
+      );
+      this.isPlaying = true;
+    },
+    pause() {
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    next() {
+      this.index++;
+      if (this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev() {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+  },
+  created() {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueScrollFixedNavbar from "vue-scroll-fixed-navbar";
 Vue.config.productionTip = false
-
+Vue.use(VueScrollFixedNavbar);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
